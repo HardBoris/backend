ebdb88cc70f7cd6cb1db3299a9672a4cf3ec5203
HardBoris <bgaibor03@gmail.com>
2023-11-21 23:27:46 -0400

    feat: docker compose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hola");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
